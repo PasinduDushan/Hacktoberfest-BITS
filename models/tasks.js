8e03443662fb349c37936e73480885275fbf2b89
CrazyBotBoy <pasindudushan07@gmail.com>
2022-09-24 05:39:13 +0000

    Spreadsheet Function Updated

--- models/tasks.js
+++ models/tasks.js
@@ -7,7 +7,14 @@ const Schema = mongoose.Schema;
     task_description: String,
     big_description: String,
     task_category: String,
-    advanceTask: Boolean
+    advanceTask: Boolean,
+    sheetData: [{userId: {
+      type: Number,
+      default: 0
+    }, sheetId: {
+      type: Number,
+      default: 1
+    }}]
 })),
   (Task = mongoose.model("Task", taskSchema));
 
